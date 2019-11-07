2bc5d23952e5c9c09f32436acf20b0c16998503c
andrei <thleontev@ya.ru>
2019-11-07 06:34:45 +0500

    v0.5

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ class CDevice(telnetlib.Telnet):
         self.ip_add = ip_add
         telnetlib.Telnet.__init__(self)
 
-
     def __del__(self):
         super().__del__()
 
@@ -55,96 +54,13 @@ with open('software.yaml') as f:
     for doc in docs:
         models = doc
 
-
 device = CDevice(ipaddress[0])
 devices.append(device)
-
-# connect to device
-try:
-    device.open()
-    device.status = "connect_success"
-except:
-    device.status = "error_connect"
-
-# check connection status !
 
 if LOGGINING:
     flog = open("log_"+device.ip_add+".txt", 'w')
 
-#check init config (for com)
-time.sleep(DELAY)
-res = device.read_very_eager().decode('utf-8')
-if re.search('initial configuration', res):
-    device.write(b"no\n")
-    time.sleep(DELAY)
-    res = device.read_very_eager().decode('utf-8')
-    if re.search('.+[>]', res) is None:
-        device.status = "error_auth_vty"
-
-#check auth to vty
-device.write(b"\n")
-time.sleep(DELAY)
-res = device.read_very_eager().decode('utf-8')
-if re.search('Password:', res):
-    device.write(PASS_AUTH.encode('ascii')+b"\n")
-    time.sleep(DELAY)
-    res = device.read_very_eager().decode('utf-8')
-    if re.search('.+[>]', res) is None:
-        device.status = "error_auth_vty"
-
-# check auth to enable
-device.write(b"en\n")
-time.sleep(DELAY)
-res = device.read_very_eager().decode('utf-8')
-if re.search('Password:', res):
-    device.write(PASS_ENA.encode('ascii') + b"\n")
-    time.sleep(DELAY)
-    res = device.read_very_eager().decode('utf-8')
-    if re.search('.+[#]', res) is None:
-        device.status = "error_auth_enable"
-    else:
-        device.status = "auth_success"
-
-# find device mode
-device.write(b"sh inv \n")
-time.sleep(DELAY)
-res = device.read_very_eager().decode('utf-8')
-template = open('cisco_ios_show_inventory.template')
-fsm = textfsm.TextFSM(template)
-result = fsm.ParseText(res)
-device.model = result[0][1]
-
-# find software version
-device.write(b"sh ver \n")
-time.sleep(DELAY)
-res = device.read_very_eager().decode('utf-8')
-template = open('cisco_ios_show_version.template')
-fsm = textfsm.TextFSM(template)
-result = fsm.ParseText(res)
-device.software = result[0][6]
-
-# cheat
-device.model = 'WS-C3750G-24TS-S1U'
-
-# find script
-if device.software != models['WS-C3750G-24TS-S1U']['software']:
-    device.software = models['WS-C3750G-24TS-S1U']['software']
-    device.script = models['WS-C3750G-24TS-S1U']['script']
-    device.file = models['WS-C3750G-24TS-S1U']['file']
-    device.update_soft = True
-
-# fill template
-file = TFTP_SERVER+"/" + device.file
-path = device.software[:re.search('.bin', device.software).start()]+"/"+device.software
-dev_temp = {'IP':device.ip_add, 'FILE': file, 'PATH': path}
-
-env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template(device.script)
-script = template.render(dev_temp).split("\n")
-
 # perform script
-device.write(b"\n")
-res = device.read_very_eager().decode('ascii')
 fwait = False
 fconfirm = True
 for line in script:
@@ -152,9 +68,96 @@ for line in script:
         if line.find(":"):
             cmd = line[2:line.find(":")]
             arg = line[line.find(":")+1:]
+            if cmd == 'connect':
+                if arg == 'start':
+                    # connect to device
+                    try:
+                        device.open()
+                        device.status = "connect_success"
+                    except:
+                        device.status = "error_connect"
+
+                    # check init config (for com)
+                    time.sleep(DELAY)
+                    res = device.read_very_eager().decode('utf-8')
+                    if re.search('initial configuration', res):
+                        device.write(b"no\n")
+                        time.sleep(DELAY)
+                        res = device.read_very_eager().decode('utf-8')
+                        if re.search('.+[>]', res) is None:
+                            device.status = "error_auth_vty"
+
+                    # check auth to vty
+                    device.write(b"\n")
+                    time.sleep(DELAY)
+                    res = device.read_very_eager().decode('utf-8')
+                    if re.search('Password:', res):
+                        device.write(PASS_AUTH.encode('ascii') + b"\n")
+                        time.sleep(DELAY)
+                        res = device.read_very_eager().decode('utf-8')
+                        if re.search('.+[>]', res) is None:
+                            device.status = "error_auth_vty"
+
+                    # check auth to enable
+                    device.write(b"en\n")
+                    time.sleep(DELAY)
+                    res = device.read_very_eager().decode('utf-8')
+                    if re.search('Password:', res):
+                        device.write(PASS_ENA.encode('ascii') + b"\n")
+                        time.sleep(DELAY)
+                        res = device.read_very_eager().decode('utf-8')
+                        if re.search('.+[#]', res) is None:
+                            device.status = "error_auth_enable"
+                        else:
+                            device.status = "auth_success"
+
+                    # find device mode
+                    device.write(b"sh inv \n")
+                    time.sleep(DELAY)
+                    res = device.read_very_eager().decode('utf-8')
+                    template = open('cisco_ios_show_inventory.template')
+                    fsm = textfsm.TextFSM(template)
+                    result = fsm.ParseText(res)
+                    device.model = result[0][1]
+
+                    # find software version
+                    device.write(b"sh ver \n")
+                    time.sleep(DELAY)
+                    res = device.read_very_eager().decode('utf-8')
+                    template = open('cisco_ios_show_version.template')
+                    fsm = textfsm.TextFSM(template)
+                    result = fsm.ParseText(res)
+                    device.software = result[0][6]
+
+                    # cheat
+                    device.model = 'WS-C3750G-24TS-S1U'
+
+                    # find script
+                    if device.software != models['WS-C3750G-24TS-S1U']['software']:
+                        device.software = models['WS-C3750G-24TS-S1U']['software']
+                        device.script = models['WS-C3750G-24TS-S1U']['script']
+                        device.file = models['WS-C3750G-24TS-S1U']['file']
+                        device.update_soft = True
+
+                    # fill template
+                    file = TFTP_SERVER + "/" + device.file
+                    path = device.software[:re.search('.bin', device.software).start()] + "/" + device.software
+                    dev_temp = {'IP': device.ip_add, 'FILE': file, 'PATH': path}
+
+                    env = Environment(loader=FileSystemLoader('templates'))
+                    template = env.get_template(device.script)
+                    script = template.render(dev_temp).split("\n")
+
+                # if connect:start
+                if arg == 'restore':
+                    device.write(b"\r\n")
+
+                # if connect:restore
+
             if cmd == 'section':
                 device.status = cmd+":"+arg
                 print(device.status)
+
             if cmd == 'wait':
                 fwait = False
                 floop = True
